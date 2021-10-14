fdf6e8ca79f7c7b1f02e414e82afc28143b4273f
jan <jan.witek88@gmail.com>
2021-10-14 18:47:13 +0200

    przed dodaniem layoutu poziomego

---
+++ app/src/main/java/com/example/myfrags/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myfrags;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private Fragment fragment1, fragment2, fragment3, fragment4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+        fragment4 = new Fragment4();
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.frame1, fragment1);
+        transaction.add(R.id.frame2, fragment2);
+        transaction.add(R.id.frame3, fragment3);
+        transaction.add(R.id.frame4, fragment4);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
